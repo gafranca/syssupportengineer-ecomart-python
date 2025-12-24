53a61859915ccf52ba220f0a3987803dc5d59264
Gabriel França <gabrifran@gmail.com>
2025-12-24 09:55:20 -0300

    Update price_plan_comparator_controller.py

--- src/controller/price_plan_comparator_controller.py
+++ src/controller/price_plan_comparator_controller.py
@@ -21,4 +21,4 @@ def compare(smart_meter_id):
 def recommend(smart_meter_id, limit=None):
     price_plan_service = PricePlanService(readings_repository)
     list_of_spend_against_price_plans = price_plan_service.get_list_of_spend_against_each_price_plan_for(smart_meter_id, limit=limit)
-    return list_of_spend_against_price_plans.reverse()
+    return list_of_spend_against_price_plans
